ac95f316cb0b8678c886f2c9813e15c32a0a9627
Xomb <tguillen25@gmail.com>
2024-04-27 08:52:46 -0400

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import './Navbar.css';
+
+export const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    return (
+        <nav>
+            <Link to="/" className='title'>
+                Home
+                </Link>
+            <div className='menu' 
+                onClick={() => {
+                    setMenuOpen(!menuOpen)
+                }}
+            >
+            <span></span>
+            <span></span>
+            <span></span>
+            </div>
+            <ul className={menuOpen ? "open" : ''}>
+                <li>
+                    <NavLink to='/about'>About</NavLink>
+                </li>
+                <li>
+                    <NavLink to='/services'>Services</NavLink>
+                    <ul>
+                        <li>
+                            <NavLink to='/services/SMSSegmentCalc'>SMS Segment Calculator</NavLink>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <NavLink to='/contact'>Contact</NavLink>    
+                </li>
+            </ul>
+        </nav>
+    )
+}
---
+++ src/components/pages/services/SMSSegmentCalc.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const SMSSegmentCalc = () => {
+    return <h1>~!SMS Segment Calculator!~</h1>
+}
